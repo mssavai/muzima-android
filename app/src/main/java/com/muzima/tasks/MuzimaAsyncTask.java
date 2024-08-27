42f63165a3e1222d51f541ad4d97ff8d3599e04e
sthaiya <sthaiya@hotmail.com>
2024-08-27 14:36:40 +0300

    Generify INPUT parameter type

--- app/src/main/java/com/muzima/tasks/MuzimaAsyncTask.java
+++ app/src/main/java/com/muzima/tasks/MuzimaAsyncTask.java
@@ -10,7 +10,10 @@
 
 package com.muzima.tasks;
 
+import android.os.AsyncTask;
 import android.util.Log;
+import androidx.annotation.MainThread;
+import androidx.annotation.WorkerThread;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -23,16 +26,19 @@ public abstract class MuzimaAsyncTask<INPUT, PROGRESS, OUTPUT> {
     private Future<OUTPUT> mFuture;
     private OnProgressListener<PROGRESS> onProgressListener;
 
-    /* @see #execute(Object) */
+    /**
+     * Convenience version of {@link #execute(Object...)} for use with a simple Runnable object.
+     * See {@link #execute(Object...)} for more information on the order of execution.
+     */
     public MuzimaAsyncTask<INPUT, PROGRESS, OUTPUT> execute() {
         return execute(null);
     }
 
     /**
-     * Starts all
-     * @param input Data you want to work with in the background
+     * Main execute function
+     * @param input Data you want to process in the background
      */
-    public MuzimaAsyncTask<INPUT, PROGRESS, OUTPUT> execute(final INPUT input) {
+    public MuzimaAsyncTask<INPUT, PROGRESS, OUTPUT> execute(final INPUT... input) {
         onPreExecute();
 
         ExecutorService executorService = AsyncWorker.getInstance().getExecutorService();
@@ -56,6 +62,18 @@ public abstract class MuzimaAsyncTask<INPUT, PROGRESS, OUTPUT> {
         return this;
     }
 
+    /**
+     * Waits if necessary for the computation to complete, and then
+     * retrieves its result.
+     *
+     * @return OUTPUT The computed result.
+     * @throws RuntimeException When get is attempted before calling {@link #execute()}
+     *
+     * <p>Usage Example:</p>
+     * <pre class="prettyprint">
+     * Int noOfObs = new DownloadEntriesTask().execute().get();
+     * </pre>
+     * */
     public OUTPUT get() {
         try {
             if (mFuture == null)
@@ -67,6 +85,20 @@ public abstract class MuzimaAsyncTask<INPUT, PROGRESS, OUTPUT> {
         }
     }
 
+    /**
+     * Waits if necessary for at most the given time for the computation to complete, and then retrieves its result.
+     *
+     * @param timeout Time to wait before cancelling the operation.
+     * @param timeUnit The time unit for the timeout.
+     *
+     * @return OUTPUT The computed result.
+     * @throws RuntimeException When get is attempted before calling {@link #execute()}
+     *
+     * <p>Usage Example:</p>
+     * <pre class="prettyprint">
+     * Int noOfObs = new DownloadEntriesTask().execute().get(2, TimeUnit.SECONDS);
+     * </pre>
+     */
     public OUTPUT get(long timeout, TimeUnit timeUnit) throws Exception {
         if (mFuture == null)
             throw new Exception("Task not executed before calling get()");
@@ -78,15 +110,13 @@ public abstract class MuzimaAsyncTask<INPUT, PROGRESS, OUTPUT> {
      * Call to publish progress from background
      * @param progress  Progress made
      */
+    @SuppressWarnings({"UnusedDeclaration"})
+    @MainThread
     protected void publishProgress(final PROGRESS progress) {
         AsyncWorker.getInstance().getHandler().post(() -> {
-            onProgress(progress);
             if (onProgressListener != null)
                 onProgressListener.onProgress(progress);
         });
-    }
-
-    protected void onProgress(final PROGRESS progress) {
     }
 
     /**
@@ -115,8 +145,9 @@ public abstract class MuzimaAsyncTask<INPUT, PROGRESS, OUTPUT> {
     }
 
     /**
-     * Work which you want to be done on UI thread before {@link #doInBackground(Object)}
+     * Work which you want to be done on UI thread before {@link #doInBackground(Object...)}
      */
+    @MainThread
     protected abstract void onPreExecute();
 
     /**
@@ -125,20 +156,24 @@ public abstract class MuzimaAsyncTask<INPUT, PROGRESS, OUTPUT> {
      * @return Output data
      * @throws Exception Any uncaught exception which occurred while working in background. If
      * any occurs, {@link #onBackgroundError(Exception)} will be executed (on the UI thread)
+     * @noinspection unchecked
      */
-    protected abstract OUTPUT doInBackground(INPUT input) throws Exception;
+    @WorkerThread
+    protected abstract OUTPUT doInBackground(INPUT... input) throws Exception;
 
     /**
-     * Work which you want to be done on UI thread after {@link #doInBackground(Object)}
-     * @param output Output data from {@link #doInBackground(Object)}
+     * Work which you want to be done on UI thread after {@link #doInBackground(Object...)}
+     * @param output Output data from {@link #doInBackground(Object...)}
      */
+    @MainThread
     protected abstract void onPostExecute(OUTPUT output);
 
     /**
      * Triggered on UI thread if any uncaught exception occurred while working in background
      * @param e Exception
-     * @see #doInBackground(Object)
+     * @see #doInBackground(Object...)
      */
+    @MainThread
     protected abstract void onBackgroundError(Exception e);
 
     public void setOnProgressListener(OnProgressListener<PROGRESS> onProgressListener) {
@@ -147,6 +182,7 @@ public abstract class MuzimaAsyncTask<INPUT, PROGRESS, OUTPUT> {
 
     private OnCancelledListener onCancelledListener;
 
+    @SuppressWarnings({"UnusedDeclaration"})
     public void setOnCancelledListener(OnCancelledListener onCancelledListener) {
         this.onCancelledListener = onCancelledListener;
     }
